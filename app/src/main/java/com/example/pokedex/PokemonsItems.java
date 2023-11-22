ecb4a1c71d1c534d66bb7410abdc254bdeb8eaae
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-11-22 13:31:55 -0600

    Initial commit

---
+++ app/src/main/java/com/example/pokedex/PokemonsItems.java
@@ -0,0 +1,14 @@
+package com.example.pokedex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PokemonsItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemons_items);
+    }
+}
